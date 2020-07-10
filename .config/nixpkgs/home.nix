dd576208c43ddba4ee9fe19318f82ed67aecf915
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2020-07-10 13:12:31 -0500

    Add home-manager file

---
+++ .config/nixpkgs/home.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, ... }:
+
+let
+  unstable = import <unstable> {};
+in {
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.03";
+
+  home.packages = [
+    unstable.protobuf3_12
+    unstable.python3
+  ];
+}
